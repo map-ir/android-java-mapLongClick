add6578f222be4ccdf0bdf130802aedce8fdde7b
Morteza Hoseini <mortezahosseinee@gmail.com>
2020-06-01 12:50:38 +0430

    initial commit

---
+++ app/src/main/java/ir/map/sample/maplongclick/MainActivity.java
@@ -0,0 +1,98 @@
+package ir.map.sample.maplongclick;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import ir.map.sdk_map.MapirStyle;
+import ir.map.sdk_map.maps.MapView;
+
+public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapLongClickListener {
+
+    MapboxMap map;
+    Style mapStyle;
+    MapView mapView;
+
+    LatLng selectedPoint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                map = mapboxMap;
+                map.setStyle(new Style.Builder().fromUri(MapirStyle.MAIN_MOBILE_VECTOR_STYLE), new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        mapStyle = style;
+                        map.addOnMapLongClickListener(MainActivity.this);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onMapLongClick(@NonNull LatLng point) {
+
+        selectedPoint = point;
+
+        Toast.makeText(this, point.getLatitude() + ", " + point.getLongitude(), Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
